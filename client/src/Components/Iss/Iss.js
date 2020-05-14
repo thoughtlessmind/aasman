b54edde5b551f313408f2af35b6eb99191f3acd9
Rajiv <1998irajeev@gmail.com>
2020-05-14 23:45:21 +0530

    Dark theme + ISS info added

--- client/src/Components/Iss/Iss.js
+++ client/src/Components/Iss/Iss.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Box, makeStyles, createStyles, Typography } from '@material-ui/core';
+import { Box, makeStyles, createStyles, Typography, CircularProgress } from '@material-ui/core';
 import axios from 'axios';
 
 const Iss  = () => {
 
+    const classes = usestyles()
     const [issData, setIssData] = useState()
 
     const getIssPosition = async () =>{
@@ -28,28 +29,52 @@ const Iss  = () => {
 
 
     return(
-        <Box style={{backgroundColor: 'blue', color: '#fff'}}>
+        <Box className={classes.mainContainer}>
             {
                 issData ?
-                    <Box>
-                        <Typography>ISS Details</Typography>
-                        <Typography>{`Currently going over city`}</Typography>
-                        <Typography>Position</Typography>
-                        <Box>
-                            <Typography>Latitude:{issData.iss_position.latitude}</Typography>
-                            <Typography>Longitude:{issData.iss_position.longitude}</Typography>
+                    <Box className={classes.subContainer}>
+                        <Typography variant='h4' align='center'>International Space Station</Typography>
+                        <Box style={{marginTop: '1.5rem', paddingLeft: '25px'}}>
+                            <Typography align='left'>{`Currently going over city`}</Typography>
+                            {/* <Typography>Position</Typography> */}
+                            <Box className={classes.issPositionContainer}>
+                                <Typography>Position: {issData.iss_position.latitude}, {issData.iss_position.longitude} (Lat, Long)</Typography>
+                            </Box>
                         </Box>
 
                     </Box>:
-                    <p >loadingData</p>
+                    <CircularProgress size={19} style={{position: 'absolute', top: '48%', left:'50%'}}/>
             }
+            <Box className={classes.subContainer}>
+                <Typography variant='h5'>People in space</Typography>
+                <Typography>{3}</Typography>
+            </Box>
         </Box>
     );
 }
 
 const usestyles = makeStyles(theme =>
     createStyles({
-
+        mainContainer:{
+            display: 'flex',
+            flexDirection: 'row',
+            width: '100%',
+            color: '#fff',
+            justifyContent: 'space-between',
+            padding: '2rem'
+        },
+        subContainer:{
+            width: '30%',
+            backgroundColor: 'rgba(0,0,0,0.7)',
+            padding: '10px',
+            borderRadius: '5px',
+            boxShadow: '1px 1px 3px 1px #848484a6'
+        },
+        issPositionContainer:{
+            display: 'flex',
+            justifyContent: 'space-between',
+            margin: '7px 0'
+        }
     })
 )
 
