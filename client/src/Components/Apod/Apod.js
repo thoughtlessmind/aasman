9264bb0d5107a8121333d9c68771fe416c997b1e
Rajiv <1998irajeev@gmail.com>
2020-05-19 23:40:19 +0530

    calls moved to class component

--- client/src/Components/Apod/Apod.js
+++ client/src/Components/Apod/Apod.js
@@ -1,48 +1,44 @@
-import React , {useState, useEffect} from 'react';
+import React , {useEffect} from 'react';
 import {
     Box,
     makeStyles,
     createStyles,
     Typography,
-    Grid,
     CircularProgress,
-    useFormControl
 } from '@material-ui/core';
 
 
-const Apod  = () => {
-    const[responseData, setResponseData] = useState([])
-    const[responseData2, setResponseData2] = useState([])
-    const [responseData3, setResponseData3] = useState([])
+const Apod  = (props) => {
+    const {apodData} = props
 
 
     const classes = usestyles()
 
-    useEffect(()=>{
-        fetch("/apod")
-            .then(res => res.json())
-            .then(data => setResponseData(data))
-    },[])
+    // useEffect(()=>{
+    //     fetch("/apod")
+    //         .then(res => res.json())
+    //         .then(data => setResponseData(data))
+    // },[])
     
-    useEffect(()=>{
-        fetch("https://api.spacexdata.com/v3/roadster")
-            .then(res => setResponseData2(res))
-            .then(res => console.log(res))
-    },[])
-
-
-    useEffect(()=>{
-        console.log(responseData2)
-    },[responseData2])
+    // useEffect(()=>{
+    //     fetch("https://api.spacexdata.com/v3/roadster")
+    //         .then(res => setResponseData2(res))
+    //         .then(res => console.log(res))
+    // },[])
 
     useEffect(()=>{
-        fetch("http://api.open-notify.org/astros.json")
-            .then(res => setResponseData3(res.body))
-    },[])
+        console.log('apodData', apodData);
+    },[apodData])
 
-    useEffect(()=>{
-        console.log(responseData3)
-    },[responseData3])
+
+    // useEffect(()=>{
+    //     fetch("http://api.open-notify.org/astros.json")
+    //         .then(res => setResponseData3(res.body))
+    // },[])
+
+    // useEffect(()=>{
+    //     console.log(responseData3)
+    // },[responseData3])
     const apiKey = 'gvLAFHhWfMdinBAuKFDs7VbBYNho9c6bHsVzCgRc'
 
 
@@ -53,11 +49,11 @@ const Apod  = () => {
         <Box className={classes.mainContainer}>
             <Box className={classes.apodContainer}>
                 {
-                    responseData.length ==0 ?
+                    !apodData ?
                         <CircularProgress className={classes.loaderStyle} size={21}/>:
-                        responseData.media_type === 'image'?
-                            <img src= {responseData.url} className={classes.apoImage} alt="nasa image of the day"/>:
-                            <embed height='400px' width='700px' src={`${responseData.url}&autoplay=1`}/>
+                        apodData.media_type === 'image'?
+                            <img src= {apodData.url} className={classes.apoImage} alt={apodData.title}/>:
+                            <embed height='400px' width='700px' src={`${apodData.url}&autoplay=1`}/>
                 }
             </Box>
             <Box>
@@ -66,10 +62,10 @@ const Apod  = () => {
                 </Typography>
                 <Box className={classes.apodTextConatiner}>
                     <Typography variant="h4" align='left' className={classes.apodTitle}>
-                        {responseData.title}
+                        {apodData.title}
                     </Typography>
                     <Typography variant='h5' align='left' id="temp" className={classes.mainText}>
-                        {responseData.explanation}
+                        {apodData.explanation}
                     </Typography>
                 </Box>
                 
--- client/src/Components/MainPage/MainPage.js
+++ client/src/Components/MainPage/MainPage.js
@@ -1,20 +1,54 @@
-import React , {useState, useEffect} from 'react';
+import React from 'react';
 import { Box } from '@material-ui/core';
 import Apod from '../Apod/Apod';
 import AppTopBar from '../AppBar/AppTopBar'
 import Iss from '../Iss/Iss';
+import axios from 'axios';
 
 
 
 
 
 class MainPage extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            issPosition: null,
+            apodData: []
+        }
+    }
+
+    getIssPosition = async () =>{
+        await axios({
+            method: 'GET',
+            url: 'http://api.open-notify.org/iss-now.json'
+        }).then(res=>{
+            this.setState({issPosition: res.data})
+        })
+    }
+
+    getApod = () =>{
+        fetch("/apod")
+            .then(res => res.json())
+            .then(data=> {
+                this.setState({apodData: data})
+            })
+    }
+
+    componentDidMount() {
+        this.getIssPosition()
+        this.getApod()
+    }
+
+
+
     render(){
         return(
             <Box>
                 <AppTopBar/>
-                <Apod/>
-                <Iss/>
+                <Apod apodData={this.state.apodData}/>
+                <Iss issPosition={this.state.issPosition}/>
             </Box>
         )
     }
